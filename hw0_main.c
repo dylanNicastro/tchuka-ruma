16469eecddafd2d9dcca30c5312c7ad024a0bbd3
dylanNicastro <145889356+dylanNicastro@users.noreply.github.com>
2024-01-29 20:27:44 +0000

    Changes made

--- hw0_main.c
+++ hw0_main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 
-void printGame(int a[]) {
-    printf("%d_%d_%d_%d_|_%d\n", a[0], a[1], a[2], a[3], a[4]);
+void printGame(int arrToPrint[]);
+
+int printPrompt();
+
+int move(int starting);
+
+int a[] = {2, 2, 2, 2, 0};
+
+int main() {
+    printGame(a);
+    int place = printPrompt();
+    int endedOn = move(place);
+    if (a[endedOn] == 0) {
+        printf("You lost because the last counter fell into section %d.\n", endedOn+1);
+        return 0;
+    }
+    else if (a[4] == 8) {
+        printf("You won!\n");
+    }
+    return 0;
+}
+
+void printGame(int arrToPrint[]) {
+    printf("%d_%d_%d_%d_|_%d\n", arrToPrint[0], arrToPrint[1], arrToPrint[2], arrToPrint[3], arrToPrint[4]);
 }
 
 int printPrompt() {
     printf("Choose a section (1-4): ");
     int choice;
     scanf("%d", &choice);
-    if (choice >= 1 && choice <= 4) {
+    if (a[choice-1] == 0) {
+        printf("Invalid choice.\n");
+        return printPrompt();
+    }
+    else if (choice >= 1 && choice <= 4) {
         return choice;
     }
     else {
@@ -17,9 +43,23 @@ int printPrompt() {
     }
 }
 
-int main() {
-    int a[] = {2, 2, 2, 2, 0};
-    printGame(a);
-    printPrompt();
-    return 0;
+int move(int starting) {
+    // convert starting to its index
+    starting--;
+    
+    int amount = a[starting];
+    a[starting] = 0;
+    starting++;
+
+    while (amount > 0) {
+        a[starting]++;
+        amount--;
+        if (starting == 4) {
+            starting = 0;
+        }
+        else {starting++;}
+    }
+    return starting;
 }
+
+
